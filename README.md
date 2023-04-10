eaeafbdb9362a5db47ea419b96dd3a6d3c9508cf
phiroun <man.phiroun@gmail.com>
2023-04-10 15:43:15 -0700

    newest update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# personal-website
+My Personal Website
 
+# Issues that need to solve
+    -small screen size
+
+# Works that still in progrees
+    -about page
+    -portfolio page
--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
 //----- Home Page -----//
 
 //alert("Hello World! Welcome to my personal homepage")
+
+var navTabs = document.getElementsById("navTabs");
+function showmenu(){
+    navTabs.style.right = "0";
+}
+function hidemenu() {
+  navTabs.style.right = "-200px";
+}
